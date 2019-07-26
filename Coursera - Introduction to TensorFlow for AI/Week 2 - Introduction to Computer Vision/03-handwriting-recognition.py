d07de9a2e7831f720024154929b8aaff49fdd137
Steven <stevenxchung@gmail.com>
2019-07-26 12:30:12 -0400

    Update 03-handwriting-recognition.py

--- Coursera - Introduction to TensorFlow for AI/Week 2 - Introduction to Computer Vision/03-handwriting-recognition.py
+++ Coursera - Introduction to TensorFlow for AI/Week 2 - Introduction to Computer Vision/03-handwriting-recognition.py
@@ -12,24 +12,39 @@ Some notes:
 I've started the code for you below -- how would you finish it?
 '''
 
-# YOUR CODE SHOULD START HERE
-# YOUR CODE SHOULD END HERE
+# Initialize imports
 import tensorflow as tf
+
+# Define callback
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoc, logs = {}):
+     if (logs.get('acc') >= 0.99):
+        print("\nReached 99% accuracy so cancelling training!")
+        self.model.stop_training = True
+
+# Load training data
 mnist = tf.keras.datasets.mnist
-
 (x_train, y_train),(x_test, y_test) = mnist.load_data()
-# YOUR CODE SHOULD START HERE
 
-# YOUR CODE SHOULD END HERE
+# Normalize data
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# Initialize callback
+callbacks = myCallback()
+
+# Define model
 model = tf.keras.models.Sequential([
-# YOUR CODE SHOULD START HERE
-    
-# YOUR CODE SHOULD END HERE
+  tf.keras.layers.Flatten(input_shape = (28, 28)),
+  tf.keras.layers.Dense(512, activation = tf.nn.relu),
+  tf.keras.layers.Dense(10, activation = tf.nn.softmax)
 ])
 
+# Compile and train the model
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-# YOUR CODE SHOULD START HERE
-# YOUR CODE SHOULD END HERE
+model.fit(x_train, y_train, epochs = 10, callbacks = [callbacks])
+
+# Evaluate model
+model.evaluate(x_test, y_test)

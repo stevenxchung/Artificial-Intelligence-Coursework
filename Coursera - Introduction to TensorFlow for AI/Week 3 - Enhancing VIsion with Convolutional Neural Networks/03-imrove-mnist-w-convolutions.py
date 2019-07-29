543cb7a250b82040936e3a045cc215ce9d54ca5f
Steven <stevenxchung@gmail.com>
2019-07-29 18:13:27 -0400

    Update 03-imrove-mnist-w-convolutions.py

--- Coursera - Introduction to TensorFlow for AI/Week 3 - Enhancing VIsion with Convolutional Neural Networks/03-imrove-mnist-w-convolutions.py
+++ Coursera - Introduction to TensorFlow for AI/Week 3 - Enhancing VIsion with Convolutional Neural Networks/03-imrove-mnist-w-convolutions.py
@@ -33,11 +33,11 @@ callbacks = myCallback()
 
 # Define model
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+  tf.keras.layers.Conv2D(64, (3, 3), activation = 'relu', input_shape = (28, 28, 1)),
   tf.keras.layers.MaxPooling2D(2, 2),
   tf.keras.layers.Flatten(),
-  tf.keras.layers.Dense(512, activation='relu'),
-  tf.keras.layers.Dense(10, activation='softmax')
+  tf.keras.layers.Dense(512, activation = 'relu'),
+  tf.keras.layers.Dense(10, activation = 'softmax')
 ])
 
 # Compile and train the model

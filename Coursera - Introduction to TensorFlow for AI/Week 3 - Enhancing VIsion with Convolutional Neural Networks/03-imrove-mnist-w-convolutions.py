f527b590f5dcd802a7ef2352d09e917102f9c68e
Steven <stevenxchung@gmail.com>
2019-07-29 17:36:02 -0400

    Update 03-imrove-mnist-w-convolutions.py

--- Coursera - Introduction to TensorFlow for AI/Week 3 - Enhancing VIsion with Convolutional Neural Networks/03-imrove-mnist-w-convolutions.py
+++ Coursera - Introduction to TensorFlow for AI/Week 3 - Enhancing VIsion with Convolutional Neural Networks/03-imrove-mnist-w-convolutions.py
@@ -21,6 +21,10 @@ class myCallback(tf.keras.callbacks.Callback):
 mnist = tf.keras.datasets.fashion_mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
+# Reshape data
+x_train = x_train.reshape(60000, 28, 28, 1)
+x_test = x_test.reshape(10000, 28, 28, 1)
+
 # Normalize data
 x_train, x_test = x_train / 255.0, x_test / 255.0
 
@@ -29,9 +33,11 @@ callbacks = myCallback()
 
 # Define model
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(input_shape = (28, 28)),
-  tf.keras.layers.Dense(512, activation = tf.nn.relu),
-  tf.keras.layers.Dense(10, activation = tf.nn.softmax)
+  tf.keras.layers.Conv2D(64, (3, 3), activation = 'relu', input_shape = (28, 28)),
+  tf.keras.layers.MaxPooling2D(2, 2),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(512, activation ='relu'),
+  tf.keras.layers.Dense(10, activation = 'softmax')
 ])
 
 # Compile and train the model

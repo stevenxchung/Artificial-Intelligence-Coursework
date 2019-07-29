c2de8dc7671fd1b6792f13852a97eb3571e36ca8
Steven <stevenxchung@gmail.com>
2019-07-29 18:13:05 -0400

    Update 03-imrove-mnist-w-convolutions.py

--- Coursera - Introduction to TensorFlow for AI/Week 3 - Enhancing VIsion with Convolutional Neural Networks/03-imrove-mnist-w-convolutions.py
+++ Coursera - Introduction to TensorFlow for AI/Week 3 - Enhancing VIsion with Convolutional Neural Networks/03-imrove-mnist-w-convolutions.py
@@ -18,7 +18,7 @@ class myCallback(tf.keras.callbacks.Callback):
       self.model.stop_training = True
 
 # Load training data
-mnist = tf.keras.datasets.fashion_mnist
+mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 # Reshape data
@@ -33,11 +33,11 @@ callbacks = myCallback()
 
 # Define model
 model = tf.keras.models.Sequential([
-  tf.keras.layers.Conv2D(64, (3, 3), activation = 'relu', input_shape = (28, 28)),
+  tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
   tf.keras.layers.MaxPooling2D(2, 2),
   tf.keras.layers.Flatten(),
-  tf.keras.layers.Dense(512, activation ='relu'),
-  tf.keras.layers.Dense(10, activation = 'softmax')
+  tf.keras.layers.Dense(512, activation='relu'),
+  tf.keras.layers.Dense(10, activation='softmax')
 ])
 
 # Compile and train the model
@@ -45,7 +45,7 @@ model.compile(optimizer = 'adam',
               loss = 'sparse_categorical_crossentropy',
               metrics = ['accuracy'])
 
-model.fit(x_train, y_train, epochs = 10, callbacks = [callbacks])
+model.fit(x_train, y_train, epochs = 20, callbacks = [callbacks])
 
 # Evaluate model
 model.evaluate(x_test, y_test)

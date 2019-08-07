291e440a48270667e1b1507d660501de6f65ee7e
Steven <stevenxchung@gmail.com>
2019-08-07 17:53:48 -0400

    Create 03-handling-complex-images.py

---
+++ Coursera - Introduction to TensorFlow for AI/Week 4 - Using Real-world Images/03-handling-complex-images.py
@@ -0,0 +1,7 @@
+'''
+Let’s now create your own image classifier for complex images. See if you can create a classifier for a set of happy or sad images that I’ve provided. Use a callback to cancel training once accuracy is greater than .999.
+
+Below is code with a link to a happy or sad dataset which contains 80 images, 40 happy and 40 sad. Create a convolutional neural network that trains to 100% accuracy on these images, which cancels training upon hitting training accuracy of >.999
+
+Hint -- it will work best with 3 convolutional layers.
+'''

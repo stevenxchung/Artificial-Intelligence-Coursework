0313dc614149fe36cef8250f850b5af248fb25a1
Steven <stevenxchung@gmail.com>
2019-08-07 17:53:53 -0400

    Update 03-handling-complex-images.py

--- Coursera - Introduction to TensorFlow for AI/Week 4 - Using Real-world Images/03-handling-complex-images.py
+++ Coursera - Introduction to TensorFlow for AI/Week 4 - Using Real-world Images/03-handling-complex-images.py
@@ -5,3 +5,73 @@ Below is code with a link to a happy or sad dataset which contains 80 images, 40
 
 Hint -- it will work best with 3 convolutional layers.
 '''
+
+import tensorflow as tf
+import os
+import zipfile
+
+
+DESIRED_ACCURACY = 0.999
+
+!wget --no-check-certificate \
+    "https://storage.googleapis.com/laurencemoroney-blog.appspot.com/happy-or-sad.zip" \
+    -O "/tmp/happy-or-sad.zip"
+
+zip_ref = zipfile.ZipFile("/tmp/happy-or-sad.zip", 'r')
+zip_ref.extractall("/tmp/h-or-s")
+zip_ref.close()
+
+class myCallback(tf.keras.callbacks.Callback):
+  def on_epoch_end(self, epoch, logs = {}):
+      if (logs.get('acc') >= DESIRED_ACCURACY):
+        print("\nReached 99.9% accuracy so cancelling training!")
+        self.model.stop_training = True
+
+callbacks = myCallback()
+
+# This Code Block should Define and Compile the Model
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Conv2D(16, (3, 3), activation = 'relu', input_shape = (300, 300, 3)),
+  tf.keras.layers.MaxPooling2D(2, 2),
+  tf.keras.layers.Conv2D(32, (3, 3), activation = 'relu'),
+  tf.keras.layers.MaxPooling2D(2, 2),
+  tf.keras.layers.Conv2D(64, (3, 3), activation = 'relu'),
+  tf.keras.layers.MaxPooling2D(2, 2),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(512, activation = 'relu'),
+  tf.keras.layers.Dense(1, activation = 'sigmoid')
+])
+
+from tensorflow.keras.optimizers import RMSprop
+
+model.compile(
+  loss = 'binary_crossentropy',
+  optimizer = RMSprop(lr = 0.001),
+  metrics = ['acc'])
+
+# This code block should create an instance of an ImageDataGenerator called train_datagen 
+# And a train_generator by calling train_datagen.flow_from_directory
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(rescale = 1./255)
+
+train_dir = "/tmp/h-or-s"
+
+train_generator = train_datagen.flow_from_directory(train_dir,
+  target_size = (300, 300),
+  batch_size = 128,
+  class_mode = 'binary')
+
+# Expected output: 'Found 80 images belonging to 2 classes'
+
+# This code block should call model.fit_generator and train for
+# a number of epochs. 
+history = model.fit_generator(
+  train_generator,
+  steps_per_epoch = 8,
+  epochs = 15,
+  verbose = 2,
+  callbacks = [callbacks])
+
+# Expected output: "Reached 99.9% accuracy so cancelling training!""
